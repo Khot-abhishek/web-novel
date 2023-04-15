d387a5010c4be18b116647eb9fb49d490aee09af
khot-abhishek <abhishekrkhot@gmail.com>
2023-04-15 20:11:23 +0530

    setting for using templates-static files at project level

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATES_DIR = BASE_DIR / 'templates'
+STATIC_DIR = BASE_DIR / 'static'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -123,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL =  "/static/"
+STATICFILES_DIRS = [STATIC_DIR]
