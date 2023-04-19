de4905d58f760a68fd2489574bd102f72457bf7c
khot-abhishek <abhishekrkhot@gmail.com>
2023-04-19 18:04:20 +0530

    overriding save_method to resize the book cover image automatically to appropriate size

--- library/models.py
+++ library/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+from PIL import Image
 
 # Create your models here.
 
@@ -35,6 +36,13 @@ class Book(models.Model):
         all_tags = self.tags.all()
         return all_tags
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.book_cover_image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.book_cover_image.path)
 
 
 
